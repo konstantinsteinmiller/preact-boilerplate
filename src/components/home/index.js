f1029bc00668d643075b5020419109dfa055fe0f
Konstantin Steinmiller <konstantin.steinmiller@nexmart.com>
2018-02-09 19:48:04 +0100

    init

---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import './home.styl';
+
+export default () => {
+	return (
+		<div class="home">
+			<h1>Home</h1>
+			<p>This is the Home component.</p>
+		</div>
+	);
+};
